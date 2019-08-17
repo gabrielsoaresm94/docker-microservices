e9e72f66e8b2d68bafe52db74f1f6bfd1481c099
Gabriel Soares <gabrielsoaresm94@gmail.com>
2019-08-17 12:58:38 -0300

    docker-compose works

--- nodejs/src/controllers/PythonController.js
+++ nodejs/src/controllers/PythonController.js
@@ -2,10 +2,11 @@ const axios = require('axios');
 
 module.exports = {
     async inputToPythom(req, res) {
+        //const url = 'http://localhost' || `0.0.0.0`
         const { message } = req.body || {};
         console.log(req.body)
         
-        const result = await axios.post('http://localhost:5000/output', { message });
+        const result = await axios.post(`0.0.0.0:5000/output`, { message });
         console.log(result.data);
         return res.status(200).json(result.data)
     }
