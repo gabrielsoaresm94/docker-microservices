d60cd2c71f836e34aa3fcfa48d5e3ae77884332c
Gabriel Soares <gabrielsoaresm94@gmail.com>
2019-08-27 07:40:15 -0300

    create route to download audio

--- nodejs/src/app/controllers/PythonController.js
+++ nodejs/src/app/controllers/PythonController.js
@@ -3,9 +3,12 @@ const axios = require('axios');
 class PythonController {
     async inputToPythom(req, res) {
         //const url = 'http://localhost' || `0.0.0.0`
-        const { message } = req.body || {};
+        const { message, name } = req.body || {};
         console.log(req.body)
-        const result = await axios.post(`http://localhost:5000/output`, { message });
+        const result = await axios.post(`http://localhost:5000/output`, { message, name });
+
+        //await axios.get(`http://localhost:5000/files/${name}`);
+
         console.log(result.data);
         if(!result.data) {
             return res.status(500).json({ message: "Bad request" });
--- python/index.py
+++ python/index.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_from_directory
 import json
-#import convert
+import os
+
+import convert
 
 app = Flask(__name__)
 
@@ -11,12 +13,17 @@ def hello_world():
 
 @app.route('/output', methods=['POST', 'GET'])
 def output():
-        error = None
-        if request.method == 'POST':
-                print (request.is_json)
-                content = request.get_json(force=True)
-                #convert.tomp3(content.get("message"), 'New_audio')
-                result = content
-                return jsonify(result)
+    error = None
+    if request.method == 'POST':
+        print (request.is_json)
+        content = request.get_json(force=True)
+        convert.tomp3(content.get("message"), content.get("name"))
+        result = content
+        return jsonify(result)
+
+
+@app.route('/files/<path:file>', methods=['GET'])
+def download(file):
+    return send_from_directory(directory='files', filename=("%s.mp3" % (file)), as_attachment=True)
 
 app.run(host='0.0.0.0', port=5000, debug=True, use_reloader=True)
